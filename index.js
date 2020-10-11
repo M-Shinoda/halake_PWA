b4caffbde0a2d22f2aca56c9cdf7c11edb9341b9
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 12:40:04 +0900

    add jquerry nad bootstrap test ver

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-$("#vibrate1").on('click',function(){
+$(".vibrate1").on('click',function(){
     //  1 秒振動
     navigator.vibrate(1000);
     });
+function vibratePattern() {
+      navigator.vibrate([100, 200, 200, 200, 500]);
+      }
