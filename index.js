37140bddf1e93172979fbfa3e382365f7ba22d49
BuildTools <unconfigured@null.spigotmc.org>
2020-10-11 15:56:59 +0900

    add jquerry nad bootstrap test ver

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ $(".vibrate1").on('click',function(){
     //  1 秒振動
     navigator.vibrate(1000);
     });
+
+var color = ''
+$('.dropdown-item').on('click', function(){
+    color =  $(this).data('id');
+});
+
+$(function() {
+    $(".change1").click(function() {
+      // cssで背景色をブルーに変更
+      $(".box1").css("background-color", color);
+    });
+    // ピンク
+    $(".change2").click(function() {
+      // cssで背景色をピンクに変更
+      $(".box2").css("background-color", color);
+    });
+    // リセット
+    $(".change3").click(function() {
+      // cssで背景色をデフォルトに変更
+      $(".box3").css("background-color", color);
+    });
+  });
